12ee3929925ce3190bb0d677bf9722ce14b1a3a2
Scott Lindsey <scott@scott.to>
2019-08-09 16:52:52 -0700

    first working deploys

---
+++ scripts/deploy_dev.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o pipefail
+set -e
+
+__here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$__here/../"
+
+. $__root/config.sh
+
+###############################################################################
+
+cd $__root;
+
+STAGE=dev
+
+echo "CloudFormation packaging..."
+aws cloudformation package \
+    --region ${REGION} \
+    --template-file sam.yml \
+    --output-template-file build/${STAGE}-packaged-template.yml \
+    --s3-bucket ${BUCKET} \
+    --s3-prefix sam/${SERVICENAME}
+
+echo "CloudFormation deploying..."
+aws cloudformation deploy \
+    --region ${REGION} \
+    --template-file build/${STAGE}-packaged-template.yml \
+    --stack-name ${SERVICENAME}-${STAGE} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-override Stage=${STAGE} \
+    --parameter-override ServiceName=${SERVICENAME}
+
